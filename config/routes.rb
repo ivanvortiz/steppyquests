03042d863300d2539557fe575ef311aad9e198b9
Vincent Ortiz <forodbagend2005@gmail.com>
2023-05-05 14:02:34 -0400

    adjusted apps/views/scenes/show.html.erb, add select option, and successfully rendered steps. modified steps_controller.rb so that can create a step with relation to quests and scenes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
   resources :quests do
-    resources :scenes
+    resources :scenes do
+      resources :steps 
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
