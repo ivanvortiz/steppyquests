33511c18f5649b12b945599cc1e4317e803a3f8c
Vincent Ortiz <forodbagend2005@gmail.com>
2023-05-19 14:34:02 -0400

    added delete function for scenes, and target steps.

--- app/controllers/scenes_controller.rb
+++ app/controllers/scenes_controller.rb
@@ -1,13 +1,26 @@
 class ScenesController < ApplicationController
   before_action :set_scene, only: %i[ show edit update destroy ]
-
+  
+  # CREATE .../scenes/1 or .../scenes/1.json
   def create
-      @quest = Quest.find(params[:quest_id])
-      @scene = @quest.scenes.create(scene_params)
-      redirect_to quest_path(@quest)
+    @quest = Quest.find(params[:quest_id])
+    @scene = @quest.scenes.create(scene_params)
+    redirect_to quest_path(@quest)
   end
 
   def show
+  end
+
+  # DELETE /quests/1 or /quests/1.json
+    def destroy
+    puts "params = #{params}"
+    @scene = Scene.find(params[:id])
+    @scene.destroy
+
+    respond_to do |format|
+      format.html { redirect_to quest_url(Quest.find(params[:quest_id])), notice: "Scene was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   private
